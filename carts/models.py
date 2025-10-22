d84d31963a533a37e367501a4cd517f3d5a8cf1e
Manoj Asher <manojasher14@gmail.com>
2025-10-22 12:45:35 +0530

    cart sessionid

--- carts/models.py
+++ carts/models.py
@@ -3,8 +3,10 @@ from store.models import Product
 
 # Create your models here.
 class Cart(models.Model):
-    cart_id = models.CharField(max_length=200, blank=True)
+    cart_id = models.CharField(max_length=250, blank=True, null=True)
+
     date_added = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.cart_id
--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render
-from .models import Product
+from store.models import Product
+from .models import Cart , Cartitem
+from django.shortcuts import redirect
 
 # Create your views here.
-def add_cart (request , product_id):
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+        return cart
+
+
+def add_cart(request , product_id):
     product = Product.objects.get(id=product_id)
-    # try:
-    #     cart = request.session['cart']
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+        )
+    cart.save()
+
+    try:
+        cart_item = Cartitem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except Cartitem.DoesNotExist:
+        cart_item = Cartitem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart
+        )
+        cart_item.save()
+    return redirect('cart')
 
 
 
